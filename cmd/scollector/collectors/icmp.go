3dbe30e0d38bfb90d9caf29fe200584be200b33a
Clint Armstrong <clint@clintarmstrong.net>
2018-03-05 16:19:22 -0500

    add rtt graphing to icmp

--- cmd/scollector/collectors/icmp.go
+++ cmd/scollector/collectors/icmp.go
@@ -7,7 +7,7 @@ import (
 
 	"bosun.org/metadata"
 	"bosun.org/opentsdb"
-	"github.com/tatsushid/go-fastping"
+	"github.com/clinta/go-ping"
 )
 
 type response struct {
@@ -31,21 +31,23 @@ func ICMP(host string) error {
 
 func c_icmp(host string) (opentsdb.MultiDataPoint, error) {
 	var md opentsdb.MultiDataPoint
-	p := fastping.NewPinger()
-	ra, err := net.ResolveIPAddr("ip4:icmp", host)
+	p, err := ping.NewPinger(host)
 	if err != nil {
 		return nil, err
 	}
-	p.AddIPAddr(ra)
-	p.MaxRTT = time.Second * 5
+	p.Count = 1
+	p.Timeout = time.Second * 5
+	p.SetPrivileged(true)
 	timeout := 1
-	p.OnRecv = func(addr *net.IPAddr, t time.Duration) {
-		Add(&md, "ping.rtt", float64(t)/float64(time.Millisecond), opentsdb.TagSet{"dst_host": host}, metadata.Unknown, metadata.None, "")
+	p.OnRecv = func(p *ping.Packet) {
+		if p == nil {
+			return
+		}
+		Add(&md, "ping.rtt", float64(p.Rtt)/float64(time.Millisecond), opentsdb.TagSet{"dst_host": host}, metadata.Unknown, metadata.None, "")
+		Add(&md, "ping.ttl", p.Ttl, opentsdb.TagSet{"dst_host": host}, metadata.Unknown, metadata.None, "")
 		timeout = 0
 	}
-	if err := p.Run(); err != nil {
-		return nil, err
-	}
+	p.Run()
 	Add(&md, "ping.timeout", timeout, opentsdb.TagSet{"dst_host": host}, metadata.Unknown, metadata.None, "")
 	return md, nil
 }
